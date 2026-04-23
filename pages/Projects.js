33a1a7ae74f6b0a06964ca8045927834de820bce
Ramaraju <venkataramaraju2005@gmail.com>
2026-04-23 17:21:49 +0530

    first commit

---
+++ pages/Projects.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+
+      <p>
+        Project 1: 
+        <a href="https://github.com/your-username/project1" target="_blank">
+          View Code
+        </a>
+      </p>
+
+      <p>
+        Project 2: 
+        <a href="https://github.com/your-username/project2" target="_blank">
+          View Code
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Projects;
